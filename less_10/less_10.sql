31a26e997fef5159760d1fbebb8df129bb6593e3
Svyatoslav <59284570+mutedalien@users.noreply.github.com>
2020-07-28 15:13:55 +0300

    less_10

--- less_10/less_10.sql
+++ less_10/less_10.sql
@@ -30,16 +30,3 @@ OK
 127.0.0.1:6379> hget emails katerina@mail.ru
 "katerina"
 */
-
-
-Task №3
-
-/*
-db.shop.insert({id: '1', 'name': 'Принтеры'},
-			   {id: '2', 'name': 'Мониторы'},
-			   {id: '3', 'name': 'Процессоры'})
-
-db.shop.insert({id: '1', 'name': 'Lexmark', 'description': 'Цветной принтер', 'price': '1000', 'catalog_id': '1', 'created_at': '2019-08-07 18:22:12', 'updated_at': '2019-08-07 18:22:12'},
-			   {id: '2', 'name': 'Sharp', 'description': 'Мощный процессор для игр и не только!', 'price': '2300', 'catalog_id': '3', 'created_at': '2019-08-07 18:22:12', 'updated_at': '2019-08-07 18:22:12'},
-			   {id: '3', 'name': 'Apple', 'description': 'Цветной монитор', 'price': '200', 'catalog_id': '2', 'created_at': '2019-08-07 18:22:12', 'updated_at': '2019-08-07 18:22:12'})
-*/
